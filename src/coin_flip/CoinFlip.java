492cc23213ada3396d185bda9024539fb55d6bc3
kvinkel <43136038+kvinkel@users.noreply.github.com>
2019-10-17 18:47:25 +0200

    Initial commit

---
+++ src/coin_flip/CoinFlip.java
@@ -0,0 +1,29 @@
+package coin_flip;
+
+import java.util.Scanner;
+
+public class CoinFlip {
+
+    public static void main(String[] args) throws InterruptedException {
+        Coin coin = new Coin(250);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Coin Flip!\nType heads or tails!\nType q to quit.");
+
+        while (true) {
+            System.out.print(">");
+            String input = sc.next();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            }
+            coin.airTime();
+            String outcome = coin.flip();
+            System.out.println("\n" + coin.toString());
+            if (input.equalsIgnoreCase(outcome)) {
+                System.out.println("You won!");
+            } else {
+                System.out.println("You lost!");
+            }
+        }
+        System.out.println("Thanks for playing!");
+    }
+}
---
+++ src/coin_flip/SideUp.java
@@ -0,0 +1,7 @@
+package coin_flip;
+
+public enum SideUp {
+    HEADS,
+    TAILS,
+    SIDE
+}
